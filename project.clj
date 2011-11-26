faa1091bf0cab715794fd3f84358f0614a559454
masaedw <masa.edw@gmail.com>
2011-11-26 16:16:35 +0900

    dependnecies

--- project.clj
+++ project.clj
@@ -1,3 +1,10 @@
 (defproject oeoe "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [compojure "0.6.5"]
+                 [hiccup "0.3.7"]
+                 [ring "0.3.11"]
+                 ]
+  :dev-dependencies [[lein-ring "0.4.5"]]
+  :ring {:handler oeoe.core/oeoe-site}
+  )
