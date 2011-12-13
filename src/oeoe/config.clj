3034b458f08a08a0db8acd7c27f520cdbbe8c3cf
masaedw <masa.edw@gmail.com>
2011-12-14 08:09:45 +0900

    config became to be created from environment variables instead of a config file

--- src/oeoe/config.clj
+++ src/oeoe/config.clj
@@ -1,4 +1,11 @@
 (ns oeoe.config
   )
 
-(load-file "etc/config.clj")
+(defmacro load-env
+  [var env]
+  `(do
+     (def ~(with-meta var {:dynamic true}) (System/getenv ~env))))
+
+(load-env *app-consumer-key* "OEOE_CONSUMER_KEY")
+(load-env *app-consumer-secret* "OEOE_CONSUMER_SECRET")
+(load-env *callback-url* "OEOE_CALLBACK_URL")
