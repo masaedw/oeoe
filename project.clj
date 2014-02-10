fb87091931aec3b801a5d1ba03e3648d59c695b7
masaedw <masa.edw@gmail.com>
2014-02-11 01:38:39 +0900

    update old packages

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject oeoe "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [compojure "0.6.5"]
-                 [hiccup "0.3.7"]
-                 [ring "0.3.11"]
-                 [twitter-api "0.6.3"]
-                 ]
+                 [compojure "1.1.6"]
+                 [hiccup "1.0.5"]
+                 [ring "1.2.1"]
+                 [twitter-api "1.0.0"]
+                 [mongoika "0.8.7"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler oeoe.core/oeoe-site}
   )
